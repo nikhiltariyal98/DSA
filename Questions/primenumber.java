49b807b187e32bb5bfd85b9ae961190641c44c57
Nikhil Tariyal <nikhil.tariyal@pl.co.za>
2025-12-17 17:01:31 +0530

    update prime

--- Questions/primenumber.java
+++ Questions/primenumber.java
@@ -8,20 +8,23 @@
         }
         static void isPrime(int a)
         {
+            if(a<=1)
+            {
+                System.out.println("Not prime");
+                return;
+            }
             int flag = 0;
             for(int i =2;i*i<=a;i++)
             {
                 if(a%i==0)
                 {
-                    flag++;
+                    
                     System.out.println("Not a prime number");
                     return;
                 }
             }
-            if(flag==0)
-            {
-                System.out.println("Prime number");
-            }
+           System.out.println("Prime number");
+            
         }
         
     }
