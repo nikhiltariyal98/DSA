ffe71bc4bf330dc21a6f5d351ff67ff2ee7f5360
Nikhil Tariyal <nikhil.tariyal@pl.co.za>
2025-12-23 00:57:10 +0530

    leetcode questions

---
+++ BinarySearch/Questions/leectcode74.java
@@ -0,0 +1,12 @@
+package BinarySearch.Questions;
+
+public class leectcode74 
+{
+    public static void main(String[] args) 
+    {
+        int[][] matrix = {{1,3,5,7},{10,11,16,20}};
+        System.out.println(matrix.length);
+        
+    }
+    
+}
--- BinarySearch/orderagnosticbinarysearch.java
+++ BinarySearch/orderagnosticbinarysearch.java
@@ -4,36 +4,14 @@ public class orderagnosticbinarysearch
     public static void main(String[] args) 
     {
         int arr[] = {3,3,3,4,5};
-        int asc = 0;
-        int desc = 0;
-        int allsame = 0;
         int start =0;
         int end = arr.length-1;
         int searched=5;
         // determine order
-        if(arr[0]<arr[arr.length-1])
-        {
-            asc++;
-        }
-        else if (arr[0]>arr[arr.length-1])
-        {
-            desc++;
-        }
-        else
-        {
-            if(arr[0]==searched)
-            {
-                return;
-            }
-            else
-            {
-                System.out.println("Not found");
-                return;
-            }
-        }
+        boolean isAsc = arr[start]<=arr[end];
         while(start<=end)
         {
-            int mid = (start+end)/2;
+            int mid = start+(end-start)/2;
             if(arr[mid]== searched)
             {
                 System.out.println("Found");
@@ -41,7 +19,7 @@ public class orderagnosticbinarysearch
             }
             else if (arr[mid]<searched)
             {
-                if(desc==1)
+                if(!isAsc)
                 {
                     end=mid-1;
                 }
@@ -53,7 +31,7 @@ public class orderagnosticbinarysearch
             }
             else
             {
-                if(desc==1)
+                if(!isAsc)
                 {
                     start=mid+1;
                 }
