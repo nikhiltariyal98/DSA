8356d570de70b6d554d2baaed373294fb59e5c75
Nikhil Tariyal <nikhil.tariyal@pl.co.za>
2026-01-14 12:36:00 +0530

    Questions

---
+++ Sorting/Leetcode/leetcode41.java
@@ -0,0 +1,40 @@
+package Sorting.Leetcode;
+
+public class leetcode41 
+{
+    public static void main(String[] args) 
+    {
+        int[]  nums = {1,2,0};
+        System.out.println(firstMissingPositive(nums));
+    }
+    static public int firstMissingPositive(int[] nums) 
+    {
+        int i =0;
+        while(i<nums.length)
+        {
+            int correct = nums[i]-1;
+            if(nums[i]<nums.length && nums[i]>0 && nums[i]!=nums[correct])
+            {
+                
+                int temp = nums[i];
+                nums[i]=nums[correct];
+                nums[correct] = temp;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        int ans = 0;
+        for(int j =0;j<nums.length;j++)
+        {
+            if(nums[j]!=j+1)
+            {
+                return j+1;
+            }
+        }
+        return nums.length+1;
+       
+    }
+    
+}
