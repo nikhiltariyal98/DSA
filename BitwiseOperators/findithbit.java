47e753872fa62d01e61dd131544eeb9440030a70
Nikhil Tariyal <nikhil.tariyal@pl.co.za>
2026-02-15 15:52:55 +0530

    Started Math for DSA

---
+++ BitwiseOperators/findithbit.java
@@ -0,0 +1,14 @@
+package BitwiseOperators;
+
+public class findithbit 
+{
+    public static void main(String[] args) 
+    {
+        int a = 5;
+        int mask = 1<<1;
+        System.out.println(a&mask);
+
+        
+    }
+    
+}
